d747c2f70b3461d69ed2e72b1151e2a0ddbc0edb
HaileyStorm <HaileyCollet@gmail.com>
2022-05-23 20:51:13 -0600

    Layout improvements, especially for Android

--- TestChart.cs
+++ TestChart.cs
@@ -10,14 +10,14 @@ namespace GaiaSphere
 {
     public class TestChart
     {
-        public LiveChartsCore.Measure.Margin Margin { get; set; } = new LiveChartsCore.Measure.Margin(40, 20, 500, 40);
+        public LiveChartsCore.Measure.Margin Margin { get; set; } = new LiveChartsCore.Measure.Margin(40, 20, 600, 40);
 
         public ISeries[] Series { get; set; }
             = new ISeries[]
             {
                 new LineSeries<double>
                 {
-                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6, 7, 10, 2, 4, 2, 1 },
                     Fill = null
                 }
             };
